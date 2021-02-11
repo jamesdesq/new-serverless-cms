397fc4060716e833651c611720cc2035ee3b1310
bitbucket-pipelines <commits-noreply@bitbucket.org>
2021-02-11 11:08:32 +0000

    Syncing to Github

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  mailSubmitUrl: '',
-  space: '1mmxh79hkgb8',
-  accessToken: 'aNcvmHGZJUxPs98d54_rKOdnuTJDlPqmXXqgB1HnLAs',
-  defaultImageUrl: '',
-};
